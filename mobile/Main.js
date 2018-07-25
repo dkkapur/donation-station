99f637647890027898074ba4464eb89b9f5bc810
Christina Kang <bikang@microsoft.com>
2018-07-24 17:22:21 -0700

    changes so far

--- mobile/Main.js
+++ mobile/Main.js
@@ -1,41 +1,68 @@
 // Main.js
 import React from 'react'
-import { StyleSheet, Platform, Image, Text, View, Button } from 'react-native'
-import firebase from 'react-native-firebase'
+        import {
+        StyleSheet, Platform, Image, Text, View, Button, View,
+                ScrollView,
+                TouchableHighlight,
+                ListView } from 'react-native'
+        import firebase from 'react-native-firebase'
+        import { FontAwesome, MaterialIcons } from '@expo/vector-icons';
+
+import {
+Text,
+        Card,
+        ButtonGroup,
+        Tile,
+        Icon,
+        ListItem,
+        Avatar,
+        } from 'react-native-elements';
+
 export default class Main extends React.Component {
-  state = { currentUser: null }
-render() {
-    const { currentUser } = this.state
-return (
-      <View style={styles.container}>
-        <Text>
-          Hi {currentUser && currentUser.email}!
-        </Text>
+    state = {currentUser: null}
+    render() {
+        const {currentUser} = this.state
+        return (
+                <ScrollView>
+        <View style={styles.headerContainer}>
+          <Icon color="white" name="invert-colors" size={62} />
+          <Text style={styles.heading}>Lists</Text>
+        </View>
+                <View style={styles.container}>
+                    <Text>
+                    Hi {currentUser && currentUser.email}!
+                    </Text>
+                
+                    <Button style={styles.topBar} onPress={this.logout.bind(this)}
+                            title="Log Out"
+                            />
+                
+                </View>
+                </ScrollView>
+                )
+    }
 
-        <Button onPress={this.logout.bind(this)}
-          title="Log Out"
-        />
+    componentDidMount() {
+        const {currentUser} = firebase.auth()
+        this.setState({currentUser})
+    }
 
-      </View>
-    )
-  }
-
-  componentDidMount() {
-    const { currentUser } = firebase.auth()
-    this.setState({ currentUser })
-  }
-
-  logout() {
-    firebase.auth().signOut().then(() => {
-      this.setState({ currentUser: null} )
+    logout() {
+        firebase.auth().signOut().then(() => {
+            this.setState({currentUser: null})
         });
-  }
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'pink'
+    },
+
+    topBar: {
+        backgroundColor: 'pink'
+    }
 })
